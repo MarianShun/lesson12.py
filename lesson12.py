5093d8b822534f489f4b2a56c45a4eaf69c0f7b3
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-03-27 23:28:54 +0300

    Update lesson12.py

--- lesson12.py
+++ lesson12.py
@@ -6,5 +6,5 @@ unique_dishes_list = list(set(stripped_dishes_list))
 # put your program here
 random_integer = random.randint(0, 1000)
 # your program’s end
-for unique_dishes_list in unique_dishes_list:
-    print(unique_dishes_list.ljust(40, '.'), random.randint(0,60), 'min')
+for unique_dishes in unique_dishes_list:
+    if unique_dishes != '': print(unique_dishes.ljust(40, '.'), random.randint(0,60), 'min')
