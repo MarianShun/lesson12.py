cf270ef58b0d2b3e756d75768b14bdd78af04f78
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-03-29 23:19:51 +0300

    Update lesson12.py

--- lesson12.py
+++ lesson12.py
@@ -3,9 +3,8 @@ dishes_string = input('What would you like? (comma required)\n')  # This is a st
 dishes_list = dishes_string.title().split(',')
 stripped_dishes_list = [i.strip() for i in dishes_list]
 unique_dishes_list = list(set(stripped_dishes_list))
-def empty_line():
-    empty_str = ''
-    return empty_str
+def time():
+    return random.randint(1, 60)
 for unique_dishes in unique_dishes_list:
-    if unique_dishes != empty_line():
-	    print(unique_dishes.ljust(40, '.'), random.randint(1,60), 'min')
+    if unique_dishes != '':
+	    print(unique_dishes.ljust(40, '.'), time(), 'min')
