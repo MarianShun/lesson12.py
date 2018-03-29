b1b7f755e4159f0ff6f0db015e8b1dbb9b33873b
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-03-29 23:17:34 +0300

    Update lesson12.py

--- lesson12.py
+++ lesson12.py
@@ -4,8 +4,8 @@ dishes_list = dishes_string.title().split(',')
 stripped_dishes_list = [i.strip() for i in dishes_list]
 unique_dishes_list = list(set(stripped_dishes_list))
 def empty_line():
-    empty_line = ''
-    return empty_line
+    empty_str = ''
+    return empty_str
 for unique_dishes in unique_dishes_list:
-    if unique_dishes != empty_line(): print(unique_dishes.ljust(40, '.'), random.randint(1,60), 'min')
-
+    if unique_dishes != empty_line():
+	    print(unique_dishes.ljust(40, '.'), random.randint(1,60), 'min')
