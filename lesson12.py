0ae5dab31e97f9c4b70aa73581db33423a176e9d
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-04-03 02:11:31 +0300

    Update lesson12.py

--- lesson12.py
+++ lesson12.py
@@ -12,8 +12,10 @@ class Dish:
 	return self.ljust(40, '.')
     def to_print(self):
 	return Dish.dots(self) + ' ' + str(Dish.calc_time()) + ' min'
+
 stripped_dishes_list = [i.strip() for i in dishes_list]
 unique_dishes_list = list(set(stripped_dishes_list))
+
 for unique_dishes in unique_dishes_list:
     if unique_dishes != '':
 	print(Dish.to_print(unique_dishes))
