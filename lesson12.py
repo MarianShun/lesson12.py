9c90b303fbe28f40da4a405a5dff4895b2c6fc6a
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-04-03 02:11:00 +0300

    Update lesson12.py

--- lesson12.py
+++ lesson12.py
@@ -9,11 +9,11 @@ class Dish:
     def calc_time():
         return random.randint(1, 100)
     def dots(self):
-	    return self.ljust(40, '.')
+	return self.ljust(40, '.')
     def to_print(self):
-	    return Dish.dots(self) + ' ' + str(Dish.calc_time()) + ' min'
+	return Dish.dots(self) + ' ' + str(Dish.calc_time()) + ' min'
 stripped_dishes_list = [i.strip() for i in dishes_list]
 unique_dishes_list = list(set(stripped_dishes_list))
 for unique_dishes in unique_dishes_list:
     if unique_dishes != '':
-	    print(Dish.to_print(unique_dishes))
+	print(Dish.to_print(unique_dishes))
