f8db3f3b23b01bcb62ea9f01e0e7d74515be20b8
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-04-24 13:38:30 +0300

    Update lesson12.py

--- lesson12.py
+++ lesson12.py
@@ -1,18 +1,7 @@
-import random
+from dish import Dish
+
 dishes_string = input('What would you like? (comma required)\n') 
 dishes_list = dishes_string.title().split(',')
-
-class Dish:
-    def __init__(self, name, time):
-        self.dishes_list = name
-        self.calc_time = time
-    def calc_time():
-        return random.randint(1, 100)
-    def dots(self):
-	return self.ljust(40, '.')
-    def to_print(self):
-	return Dish.dots(self) + ' ' + str(Dish.calc_time()) + ' min'
-
 stripped_dishes_list = [i.strip() for i in dishes_list]
 unique_dishes_list = list(set(stripped_dishes_list))
 
